9f0d9ee29a4e3c8286f00b4ce7dcdce1c1544344
Brad Hoover <hoov1185@gmail.com>
2013-03-29 09:43:47 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>REvival 41</title>
+        <meta name="description" content="REvival 41 tells the story of Trey's comeback, his attempt to reconcile the tragedy of his past and the pain of his present, with his unshakeable belief for the future. It's the human struggle to persist, continue and hope in the face of overwhelming pain.">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Facebook Open Graph Info -->
+        <meta property="og:title" content="REvival 41" />
+        <meta property="og:url" content="http://revival41.com" />
+        <meta property="og:description" content="REvival 41 tells the story of Trey's comeback, his attempt to reconcile the tragedy of his past and the pain of his present, with his unshakeable belief for the future. It's the human struggle to persist, continue and hope in the face of overwhelming pain." />
+        <meta property="og:image" content="img/re_logo.jpg" />
+        <!-- End Facebook Open Graph Info -->
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/style.css?v=3.0">
+        <link rel="shortcut icon" href="favicon.ico?v=1.1">
+
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/javascript.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
+        <div id="home_anchor"></div>
+        
+        <div id="container">
+            <div id="header_wrapper">
+                <div id="header_right">
+                <ul class="social">
+                    <li><div class="fb-like" data-href="http://www.revival41.com" data-send="false" data-layout="button_count" data-width="90" data-show-faces="false" data-font="tahoma"></div>
+                    <li><a href="https://twitter.com/share" class="twitter-share-button" data-text="Watch the new @treycanard film &quot;REvival 41&quot;">Tweet</a>
+                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                </ul>
+            </div>
+
+            <div id="header_left">
+                <ul class="nav">
+                    <li><a href="#home_anchor">HOME</a>
+                    <li><a href="#synopsis_anchor">SYNOPSIS</a>
+                    <li><a href="#download_anchor">DOWNLOAD</a>
+                </ul>
+            </div>           
+        </div>
+
+            <div id="home">
+                <div id="video">
+                    <iframe width="100%" height="100%" src="http://www.youtube.com/embed/fSaSewBnC_U?autoplay=1&rel=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div id="home_hotspot"><a href="http://uxentertainment.com" class="hotspot">&nbsp</a></div>
+            </div>
+
+            <div id="synopsis">
+                <div id="synopsis_anchor"></div>
+                <div id="synopsis_text">
+                    <p>
+                        Trey Canard’s crash last season was one that most riders don’t come back from.  But #41 knew he had a higher calling.  When others doubted and the pain progressed, he knew he was destined to race again.  This is the story of Trey’s year-long journey from back injury to the starting gate of the 2013 Supercross season as he strives to put #41 back on top. 
+                    </p>
+
+                    <p>
+                        REvival 41 tells the story of Trey's comeback, his attempt to reconcile the tragedy of his past and the pain of his present, with his unshakeable belief for the future. It's the human struggle to persist, continue and hope in the face of overwhelming pain. 
+                    </p>
+
+                    <p>
+                        <i>"People say that what you believe in is a fairy tale. But I don't believe that at all...I believe that I was put on this earth to ride a motorcycle."</i> -Trey Canard
+                    </p>
+                </div>
+                <div id="synopsis_hotspot"><a href="http://uxentertainment.com" class="hotspot">&nbsp</a></div>
+            </div>
+
+            <div id="download">
+                <div id="download_anchor"></div>
+                <div id="download_hotspot"><a href="http://uxentertainment.com" class="hotspot">&nbsp</a></div>
+            </div>
+        </div>
+
+    </body>
+</html>
